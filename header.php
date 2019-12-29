033a2399c48b2fa2300a912b0c439152e9a08fb7
Grega Apat <48824721+grega14@users.noreply.github.com>
2019-12-29 08:31:45 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php wp_head();?>
+
+	<title>Costum Theme Tutorial</title>
+	
+</head>
+<body <?php body_class();?>>
+
+
+<header class="sticky-top">
+	
+</header>
